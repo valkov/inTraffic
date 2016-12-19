52d46dd95f961971df907a762a2d39f3a91d78dc
Valentin Kovalski <valentin@sensibo.com>
2016-12-19 15:15:32 +0200

    Initial Commit

---
+++ inTraffic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  inTraffic
+//
+//  Created by Valentyn Kovalsky on 12/19/16.
+//  Copyright © 2016 Valentyn Kovalsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ inTraffic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  inTraffic
+//
+//  Created by Valentyn Kovalsky on 12/19/16.
+//  Copyright © 2016 Valentyn Kovalsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
